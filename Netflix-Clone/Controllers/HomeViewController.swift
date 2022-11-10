75fff0d1fba03a50717712229d49f7cc3bba4c76
User-P2 <user-p2@w14-2.macminivault.com>
2022-11-10 16:32:24 -0700

    Primary Commit

---
+++ Netflix-Clone/Controllers/HomeViewController.swift
@@ -0,0 +1,32 @@
+//
+//  HomeViewController.swift
+//  Netflix-Clone
+//
+//  Created by User-P2 on 11/10/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        setUI()
+    }
+    
+    private func setUI() {
+        let button: UIButton = {
+            let button = UIButton()
+            button.setTitle("I'm Home", for: .normal)
+            button.backgroundColor = .black
+            button.frame = CGRect(x: self.view.center.x / 2, y: self.view.center.y, width: 200, height: 100)
+            return button
+        }()
+        
+        let view = UIView()
+        view.addSubview(button)
+        self.view.addSubview(view)
+    }
+
+}
---
+++ Netflix-Clone/MainTabBarViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Netflix-Clone
+//
+//  Created by User-P2 on 11/10/22.
+//
+
+import UIKit
+
+final class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        
+        let homeViewController = UINavigationController(rootViewController: HomeViewController())
+        let upCommingViewController = UINavigationController(rootViewController: UpCommingViewController())
+        let searchViewController = UINavigationController(rootViewController: SearchViewController())
+        let downloadViewController = UINavigationController(rootViewController: DownloadViewController())
+        
+        homeViewController.title = "Home"
+        homeViewController.tabBarItem.image = UIImage(systemName: "house")
+        
+        upCommingViewController.title = "Up Comming"
+        upCommingViewController.tabBarItem.image = UIImage(systemName: "play")
+        
+        searchViewController.title = "Search Movie"
+        searchViewController.tabBarItem.image = UIImage(systemName: "magnifyingglass")
+        
+        downloadViewController.title = "Downloads"
+        downloadViewController.tabBarItem.image = UIImage(systemName: "arrow.down.to.line")
+        
+        setViewControllers([homeViewController, upCommingViewController, searchViewController, downloadViewController], animated: true)
+         
+    }
+
+}
+
